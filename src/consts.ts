9d08e9416b33506da9b2f3ddeca2eb4474c2c2d6
Manav Panchal <93885954+manav03panchal@users.noreply.github.com>
2026-09-08 12:20:00 -0700

    Build Otis elevator services site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Otis Elevator Services | Toronto Elevator Service";
+export const SITE_DESCRIPTION = "Otis Elevator Services provides dependable elevator maintenance, modernization, emergency response, and inspections across Toronto and the GTA.";
